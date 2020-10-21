a70c50ae55babca9def2d9186ffa40bf08505017
Sijia Yin <yinsijia1617@163.com>
2020-10-21 13:36:53 -0600

    Initial Commit

---
+++ Recipe/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Recipe
+//
+//  Created by 印思佳 on 2020-10-21.
+//  Copyright © 2020 印思佳. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
